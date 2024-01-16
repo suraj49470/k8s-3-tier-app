c910eaf582dc4fb12bd972f52e796c32ded224a7
Suraj Singh <suraj49470@gmail.com>
2024-01-16 15:50:22 +0530

    test

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-
+console.log(process.env);
 export const host = process.env.REACT_APP_BACKEND_URL;
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
