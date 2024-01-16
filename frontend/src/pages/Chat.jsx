0587114da95d2b2cdc05cfbb0cc871de51e70eec
Suraj Singh <suraj49470@gmail.com>
2024-01-16 18:39:55 +0530

    socket error fiex

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -27,7 +27,7 @@ export default function Chat() {
   }, []);
   useEffect(() => {
     if (currentUser) {
-      socket.current = io(host);
+      socket.current = io(host+"/api");
       socket.current.emit("add-user", currentUser._id);
     }
   }, [currentUser]);
